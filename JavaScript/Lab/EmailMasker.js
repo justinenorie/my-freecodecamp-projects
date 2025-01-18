99636d63e3db79213981ae24c86c4b442e92b412
justinenorie <justinedelacruz97@gmail.com>
2025-01-18 13:59:28 +0800

    Update the explanation to understand it easily

--- JavaScript/Lab/EmailMasker.js
+++ JavaScript/Lab/EmailMasker.js
@@ -1,29 +1,30 @@
-// Email Masker Showing the First and Last letter while hiding the actual email without hiding the address of the email
+// Email Masker: Shows the first and last letters while masking the rest of the username.
+// Keeps the domain address (e.g., "@example.com") visible.
 
-// Function for getting the email and mask it
 const maskEmail = (email) => {
-    let hide = "*"; // The text to hide
+    let hide = "*"; // Text used to hide parts of the email.
 
-    //Using the slice method to remove the first and last part of the selected text
+    // Extracting the username part before "@".
+    // For example "justine@example.com", this will select the "justine".
+    const textFinder = email.slice(0, email.indexOf("@"));
 
-    //Selecting the text of the email from start until it reaches the @ part of the email eg. justine@gmail.com it will only select the justine
-    const textFinder = email.slice(0, ( email.indexOf("@") ));
-    
-    //This will select the email from @ to last letter eg."@gmail.com"
-    const emailCheck = email.slice( email.indexOf("@") ); 
+    // Extracting the domain address including "@".
+    // For example "justine@example.com", this will select the "@example.com".
+    const emailCheck = email.slice(email.indexOf("@"));
 
-    // This is the method for selecting the First and Last letter
-    const showFirstandLast = textFinder.slice(1, ( email.indexOf("@") - 1 ));
-    
-    //Replacing the text of the email with "*" except for first and last letter of the email
+    // Extracting the first and last part of the username to be masked.
+    // For example "justine", this selects everything except the first and last characters: "usti".
+    const showFirstandLast = textFinder.slice(1, textFinder.length - 1);
+
+    // Replace the username into "*" while keeping the first and last characters unchanged.
     const textToHide = textFinder.replace(showFirstandLast, hide.repeat(showFirstandLast.length));
 
-    //Outputing the result of the replaced text and address of the email
+    // Output of the result of username and the domain address.
     return textToHide + emailCheck;
-}
+};
 
 let email = "apple.pie@example.com";
-console.log(maskEmail(email));
+console.log(maskEmail(email)); // Output: "a*******e@example.com"
 
 email = "freecodecamp@example.com";
-console.log(maskEmail(email));
+console.log(maskEmail(email)); // Output: "f***********p@example.com"
